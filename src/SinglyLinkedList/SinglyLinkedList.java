01036056be2f625470fa8771c8e49506f74248f5
Prem <prempawar28@gmail.com>
2022-06-03 09:28:33 +0530

    Search an element in singly linked list

--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -1,5 +1,9 @@
 package SinglyLinkedList;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.util.List;
+
 public class SinglyLinkedList {
     private ListNode head;
 
@@ -14,6 +18,7 @@ public class SinglyLinkedList {
     }
 
 
+    //Display the LinkedList
     public void display() {
         ListNode current = head;
         while(current != null){
@@ -23,12 +28,14 @@ public class SinglyLinkedList {
         System.out.print("null");
     }
 
+    //Add new node at the beginning of the linked list
     public void Beginning(int value) {
         ListNode newNode = new ListNode(value);
         newNode.next = head;
         head = newNode;
     }
 
+    //Add new node at end of the linked list
     public void End(int value) {
         ListNode newNode = new ListNode(value);
         if(head == null){
@@ -41,22 +48,64 @@ public class SinglyLinkedList {
         current.next = newNode;
     }
 
+    //add new node at given position in linked list
+    public void Insert(int value, int position){
+        ListNode node = new ListNode(value);
+        if(position == 1){
+            node.next = head;
+            head = node;
+        }
+        ListNode previous = head;
+        int count = 1;
+        while (count < position -1){
+            previous = previous.next;
+            count++;
+        }
+        ListNode current = previous.next;
+        node.next = current;
+        previous.next = node;
+    }
+
+    //Search Element in Singly linked list
+    public boolean Search(ListNode head, int valueToSearch) {
+        if(head == null){
+            return false;
+        }
+        ListNode curr = head;
+        while(curr != null){
+            if(curr.data == valueToSearch){
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
 
-        sll.head = new ListNode(10);
+        ListNode head = new ListNode(10);
         ListNode second = new ListNode(12);
         ListNode third = new ListNode(14);
         ListNode fourth = new ListNode(16);
 
-        sll.head.next = second;
+        head.next = second;
         second.next = third;
         third.next = fourth;
 
         //sll.display();
 
         //sll.Beginning(8);
-        sll.End(18);
-        sll.display();
+        //sll.End(18);
+        //sll.Insert(22,3);
+        //sll.display();
+
+        if(sll.Search(head, 8)){
+            System.out.println("Search key Found");
+        }else {
+            System.out.println("Search ket not Found");
+        }
+
+
     }
 }
