bf20518e1d27183cc31aba5498ed443feb28752c
Prem <prempawar28@gmail.com>
2022-06-13 08:14:31 +0530

    Generic code for Doubly Linked List

---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,33 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    private class ListNode{
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+
+    //check weather linkt is empty or not
+    public boolean isEmpty(){
+        return length === 0;
+    }
+    
+    public int length(){
+        return length;
+    }
+}
