0ac5061eae6ef3cd325dc38c847072d719090c00
Prem <prempawar28@gmail.com>
2022-06-13 08:43:24 +0530

    Print DLL in forward and backward direction

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,9 @@
 package DoublyLinkedList;
 
+import com.sun.org.apache.xerces.internal.util.SymbolTable;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 public class DoublyLinkedList {
     private ListNode head;
     private ListNode tail;
@@ -24,10 +28,51 @@ public class DoublyLinkedList {
 
     //check weather linkt is empty or not
     public boolean isEmpty(){
-        return length === 0;
+        return length == 0;
     }
     
     public int length(){
         return length;
+    }
+
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            head = newNode;
+        }else{
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void printForwardDirection(){
+        ListNode temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void printBackwardDirection(){
+        ListNode temp = tail;
+        while (temp != null){
+            System.out.print(temp.data + "-->");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args){
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertLast(3);
+        dll.insertLast(7);
+        dll.insertLast(9);
+        dll.insertLast(5);
+
+        dll.printForwardDirection();
+        dll.printBackwardDirection();
     }
 }
